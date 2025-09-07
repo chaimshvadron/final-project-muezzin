144aa82f98f9b65ed70c312482d1e1fe808a605b
ChaimShvadron <s0527649958@gmail.com>
2025-09-07 15:25:01 +0300

    Refactor FileMetadataCollector to improve metadata collection and add JSON serialization; add .gitignore file

--- services/FileCollector/metadata.py
+++ services/FileCollector/metadata.py
@@ -1,22 +1,33 @@
 import os
+import json
+from pydub import AudioSegment
 
-class FileMetadata:
+class FileMetadataCollector:
     def __init__(self, file_path):
         self.file_path = file_path
 
-    def get_metadata(self):
+    def collect_metadata(self):
         try:
             state = os.stat(self.file_path)
-            return {
+            audio = AudioSegment.from_file(self.file_path)
+            metadata = {
                 "name": os.path.basename(self.file_path),
                 "size": state.st_size,
                 "last_modified": state.st_mtime,
                 "last_accessed": state.st_atime,
                 "creation_time": state.st_birthtime,
+                "audio_length": len(audio) / 1000.0,
             }
+            return metadata
         except FileNotFoundError:
             print(f"File not found: {self.file_path}")
             return None
         except Exception as e:
             print(f"Error accessing {self.file_path}: {e}")
             return None
+
+    def to_json(self):
+        metadata = self.collect_metadata()
+        if metadata is not None:
+            return json.dumps(metadata)
+        return None
