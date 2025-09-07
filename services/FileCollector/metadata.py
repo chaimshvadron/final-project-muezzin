8b6ae77ea314b11e2697b28b55c6736394a280ac
ChaimShvadron <s0527649958@gmail.com>
2025-09-07 13:00:41 +0300

    Add FileMetadata class to retrieve file metadata

--- services/FileCollector/metadata.py
+++ services/FileCollector/metadata.py
@@ -1,4 +1,22 @@
-from datetime import datetime
+import os
 
+class FileMetadata:
+    def __init__(self, file_path):
+        self.file_path = file_path
 
-print("current time:", datetime.now().strftime("%Y%m%d-%H%M%S"))
+    def get_metadata(self):
+        try:
+            state = os.stat(self.file_path)
+            return {
+                "name": os.path.basename(self.file_path),
+                "size": state.st_size,
+                "last_modified": state.st_mtime,
+                "last_accessed": state.st_atime,
+                "creation_time": state.st_birthtime,
+            }
+        except FileNotFoundError:
+            print(f"File not found: {self.file_path}")
+            return None
+        except Exception as e:
+            print(f"Error accessing {self.file_path}: {e}")
+            return None
